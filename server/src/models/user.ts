4427e7fc5665a974d80839d4a07c7d874c9247d5
chahtk <chahtk@gmail.com>
2020-11-24 13:59:23 +0900

    feat: user model schema
    - user 모델 스키마 작성

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,8 +1,31 @@
 import mongoose from 'mongoose';
 
+const notiOptions = new mongoose.Schema({
+  like: String,
+  comment: String,
+  commentLike: String,
+});
+
+const notiContents = new mongoose.Schema({
+  userId: mongoose.Schema.Types.ObjectId,
+  notiType: String,
+  date: String,
+});
+
+const followSchema = new mongoose.Schema({
+  userId: mongoose.Schema.Types.ObjectId,
+});
+
 const userSchema = new mongoose.Schema({
   name: String,
-  username: String,
+  userName: String,
+  email: String,
+  password: String,
+  profileImg: String,
+  follow: [followSchema],
+  follower: [followSchema],
+  notiOptions,
+  notiContents,
 });
 
 export default mongoose.model('User', userSchema);
