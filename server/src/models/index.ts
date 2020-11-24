f98bfe81aa781bdefb8770ef013c75e842f9aa44
chahtk <chahtk@gmail.com>
2020-11-24 14:26:17 +0900

    refactor: model들을 하나의 객체로 관리
    - models/index.ts 에서 해당 디렉토리의 다른 모델들을 객체로 관리

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,33 +1,11 @@
-/* eslint-disable no-console */
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import './user';
-import './feed';
-import './dm';
+import User from './user';
+import Feed from './feed';
+import Dm from './dm';
 
-function connectMongoose(): void {
-  dotenv.config();
-  const DB_PATH = process.env.DB_PATH || 'err';
+const db = {
+  User,
+  Feed,
+  Dm,
+};
 
-  mongoose.connect(
-    DB_PATH,
-    {
-      useCreateIndex: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
-    (err) => {
-      if (err) {
-        return console.error('database connection error', err);
-      }
-      return console.log('database connected...');
-    },
-  );
-}
-
-function connectCollections(): void {
-  connectMongoose();
-  mongoose.connection.on('disconnected', connectMongoose);
-}
-
-export default connectCollections;
+export default db;
