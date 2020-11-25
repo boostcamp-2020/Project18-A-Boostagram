e40ce852493de0579eaf3047c3402e9bb6f8aa59
chahtk <chahtk@gmail.com>
2020-11-25 19:03:50 +0900

    feat: feed(post) routing
    - feedController를 import하여 경로 설정

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { indexController } from '../controllers';
 import userRouter from './user.router';
+import feedRouter from './feed.router';
 
 const router = express.Router();
 
 router.get('/', indexController);
 router.use('/user', userRouter);
+router.use('/feed', feedRouter);
 
 export default router;
