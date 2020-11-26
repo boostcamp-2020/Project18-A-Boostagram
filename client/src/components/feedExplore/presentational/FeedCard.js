76652aae010dd23d4a11884dcf3fc85592c18d33
BK <rlaqudrnr810@naver.com>
2020-11-26 18:42:56 +0900

    feat: 호버 기능
    - 마우스 호버 기능 구현
    - 마우스 호버 시 해당 게시글의 좋아요 / 코멘트 갯수 출력
    - 아이콘 위치 / 코멘트 아이콘 등 수정 예정

--- client/src/components/feedExplore/presentational/FeedCard.js
+++ client/src/components/feedExplore/presentational/FeedCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const style = {};
@@ -12,20 +12,68 @@ style.FeedCard = styled.article`
   cursor: pointer;
 `;
 
+style.iconImg = styled.img`
+  height: 15px;
+  width: 15px;
+  margin: auto 0;
+`;
+
+style.number = styled.div`
+  margin: auto 0;
+`;
+
 style.ImgBox = styled.img`
   max-width: 100%;
   height: auto;
   display: block;
-  opacity: 1;
+  opacity: ${(props) => (props.hover ? 0.3 : 1)};
+`;
+
+style.icon = styled.div`
+  opacity: ${(props) => {
+    console.log(props);
+    return props.hover ? 1 : 0;
+  }};
+  display: flex;
+  text-align: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 `;
 
 const FeedCard = (input) => {
-  const { imgUrl } = input.data;
+  const { feedImg, like, comments } = input.data;
+  const [hover, setHover] = useState(false);
+  const likeIcon =
+    'https://user-images.githubusercontent.com/39620410/100316277-82a4c580-2ffd-11eb-8f22-704e6cb6d29f.png';
+  const likeNum = like.length;
+  const commentIcon =
+    'https://user-images.githubusercontent.com/39620410/100316277-82a4c580-2ffd-11eb-8f22-704e6cb6d29f.png';
+  const commentNum = comments.length;
 
+  const hoverHandler = () => {
+    setHover(!hover);
+  };
+
+  const clickHandler = () => {
+    // todo: 상세 화면 출력
+  };
   return (
     <>
-      <style.FeedCard>
-        <style.ImgBox src={imgUrl[0]} />
+      <style.FeedCard
+        onMouseEnter={hoverHandler}
+        onMouseLeave={hoverHandler}
+        onClick={clickHandler}
+      >
+        <style.ImgBox src={feedImg[0]} hover={hover} />
+        <style.icon hover={hover}>
+          <style.iconImg src={likeIcon} />
+          <style.number>{likeNum}</style.number>
+          <style.iconImg src={commentIcon} />
+          <style.number>{commentNum}</style.number>
+        </style.icon>
       </style.FeedCard>
     </>
   );
