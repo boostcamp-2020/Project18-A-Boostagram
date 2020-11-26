7cc5b7194075cfe45cb7cf49037e3c8be7e4cb3d
JuHyeon Lee <leon6095@gmail.com>
2020-11-26 16:07:42 +0900

    feat: 상단바 UI 구현 및 페이지 연결
    - 로고 영역, 검색바 영역, 네비게이션바 영역으로 구분
    - 로고, 검색바, 네비게이션바 css 설정
    - constants 내 icon.js 파일에 svg 컴포넌트들 추가
    - 로고 및 네비게이션바 아이콘들 클릭 시 페이지 이동 구현

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -1,24 +1,84 @@
 import React from 'react';
 import styled from 'styled-components';
+import icon from '@constants/icon';
 
 const style = {};
 
-style.Header = styled.div`
-  display: inline-flex;
-  justify-content: space-between;
+style.HeaderContainer = styled.div`
+  width: 100%;
   height: 54px;
   border: 1px solid ${(props) => props.theme.color.border};
   position: absolute;
   top: 0;
-  width: 100%;
+`;
+
+style.Header = styled.div`
+  width: 935px;
+  height: 54px;
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+`;
+
+style.LogoArea = styled.a`
+  flex: 2;
+`;
+
+style.Logo = styled.img`
+  margin-top: 7px;
+`;
+
+style.SearchBar = styled.input`
+  height: 20px;
+  flex: 1;
+  background-color: ${(props) => props.theme.color.background};
+  border: 1px solid ${(props) => props.theme.color.border};
+  border-radius: 3px;
+  text-align: center;
+  padding: 3px;
+`;
+
+style.NavigationBar = styled.div`
+  flex: 2;
+  display: flex;
+  justify-content: flex-end;
+  a:not(:first-child) {
+    margin-left: 22px;
+  }
 `;
 
 const Header = () => (
-  <style.Header>
-    <div>1</div>
-    <div>2</div>
-    <div>3</div>
-  </style.Header>
+  <style.HeaderContainer>
+    <style.Header>
+      <style.LogoArea href="/">
+        <style.Logo
+          alt="Instagram"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+        />
+      </style.LogoArea>
+      <style.SearchBar type="text" placeholder="검색" />
+      <style.NavigationBar>
+        <a href="/">
+          <icon.Home />
+        </a>
+        <a href="/newFeed">
+          <icon.NewFeed />
+        </a>
+        <a href="/">
+          <icon.Direct />
+        </a>
+        <a href="/explore">
+          <icon.Explore />
+        </a>
+        <a href="/">
+          <icon.Noti />
+        </a>
+        <a href="/">
+          <icon.Profile />
+        </a>
+      </style.NavigationBar>
+    </style.Header>
+  </style.HeaderContainer>
 );
 
 export default Header;
