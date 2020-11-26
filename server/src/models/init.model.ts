fdaf297adb09d0232c95d7913f5e7254304b9a19
JuHyeon Lee <leon6095@gmail.com>
2020-11-26 18:21:55 +0900

    fix: user router 삭제
    - 아무 기능도 수행하지 않아 삭제함

--- server/src/models/init.model.ts
+++ server/src/models/init.model.ts
@@ -2,6 +2,9 @@
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
+import UserModel from './user.model';
+import FeedModel from './feed.model';
+
 function connectMongoose(): void {
   dotenv.config();
   const DB_PATH = process.env.DB_PATH || 'err';
@@ -22,9 +25,68 @@ function connectMongoose(): void {
   );
 }
 
+function seedData(): void {
+  UserModel.collection.drop();
+  UserModel.create([
+    {
+      name: 'JuHy',
+      userName: 'JuHyeon-Lee',
+      email: 'leon6095@gmail.com',
+      password: '1q2w3e4r',
+      profileImg:
+        'https://ca.slack-edge.com/T019JFET9H7-U019PN65926-7d7d0974a7f9-512',
+    },
+    {
+      name: 'HyoJoon',
+      userName: 'chahtk',
+      email: 'chahtk@gmail.com',
+      password: '1q2w3e4r!',
+      profileImg:
+        'https://ca.slack-edge.com/T019JFET9H7-U01AE19CWUQ-ccf648c8636a-512',
+    },
+    {
+      name: 'ByeongGook',
+      userName: 'rlaqudrnr810',
+      email: 'rlaqudrnr810@gmail.com',
+      password: '1q2w3e4r!@',
+      profileImg:
+        'https://ca.slack-edge.com/T019JFET9H7-U019GGE0086-9b1c9fa14e8b-512',
+    },
+  ]);
+
+  FeedModel.collection.drop();
+  FeedModel.create([
+    {
+      author: {
+        userId: '5fbf7104e5213064cc2ffae7',
+        name: 'JuHy',
+        userName: 'JuHyeon-Lee',
+        profileImg:
+          'https://ca.slack-edge.com/T019JFET9H7-U019PN65926-7d7d0974a7f9-512',
+      },
+      content: 'yageunjoa',
+      feedImg:
+        'https://avatars0.githubusercontent.com/u/24909656?s=460&u=d41305b6b314f0232605a0f9f293da5f40319199&v=4',
+    },
+    {
+      author: {
+        userId: '5fbf7104e5213064cc2ffae8',
+        name: 'HyoJoon',
+        userName: 'chahtk',
+        profileImg:
+          'https://ca.slack-edge.com/T019JFET9H7-U01AE19CWUQ-ccf648c8636a-512',
+      },
+      content: 'yageunsireo',
+      feedImg:
+        'https://ca.slack-edge.com/T019JFET9H7-U01AE19CWUQ-ccf648c8636a-512',
+    },
+  ]);
+}
+
 function connectCollections(): void {
   connectMongoose();
   mongoose.connection.on('disconnected', connectMongoose);
+  seedData();
 }
 
 export default connectCollections;
