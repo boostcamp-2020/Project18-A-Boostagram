301f4b3bbbde04cf821dd4fef5414bf7c069d74c
BK <rlaqudrnr810@naver.com>
2020-11-27 10:12:27 +0900

    fix: constants 경로 수정
    - constants  경로 수정

--- client/src/components/feedExplore/presentational/FeedCard.js
+++ client/src/components/feedExplore/presentational/FeedCard.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import icon from '../../../constants/icon';
+import icon from '@constants/icon';
 
 const style = {};
 
