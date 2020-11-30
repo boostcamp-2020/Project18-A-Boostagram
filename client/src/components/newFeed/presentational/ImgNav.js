da5ae6ba581ae25d628df9db28a9920f31f9310c
chahtk <chahtk@gmail.com>
2020-11-30 22:19:26 +0900

    feat: image navbar
    - 이미지 하단에 이미지 개수만큼 동그라미 모양이 있음
    - 선택된 이미지 index의 동그라미는 흰색으로 표시

---
+++ client/src/components/newFeed/presentational/ImgNav.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const style = {};
+style.NavBar = styled.ul`
+  position: absolute;
+  bottom: 5px;
+  display: flex;
+  width: 100%;
+  list-style-type: none;
+`;
+style.NavItem = styled.li`
+  background-color: ${(props) =>
+    props.active ? 'white' : props.theme.color.background};
+  width: 8px;
+  height: 8px;
+  border-radius: 8px;
+  & + & {
+    margin-left: 5px;
+  }
+`;
+
+const ImgNav = (props) => {
+  const { imgIndex, imgs } = props;
+  return (
+    <style.NavBar>
+      {imgs.map((img, index) => {
+        const key = img + index;
+        return imgIndex === index ? (
+          <style.NavItem key={key} active />
+        ) : (
+          <style.NavItem key={key} />
+        );
+      })}
+    </style.NavBar>
+  );
+};
+
+ImgNav.propTypes = {
+  imgIndex: PropTypes.number.isRequired,
+  imgs: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default ImgNav;
