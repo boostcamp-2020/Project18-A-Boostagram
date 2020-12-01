22d142975ce84be869c6ea8628201aef936aebc2
chahtk <chahtk@gmail.com>
2020-12-01 14:51:52 +0900

    feat: multer 모듈로 파일 파싱
    - multer 설치
    - multer를 이용한 파일 파싱 및 저장

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,18 +3,29 @@ import dotenv from 'dotenv';
 import express from 'express';
 import logger from 'morgan';
 import cors from 'cors';
+import multer from 'multer';
 import initDB from './models/init.model';
 import index from './routes/index';
 
 dotenv.config();
 
 const port = process.env.PORT || 3000;
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, 'uploads/');
+  },
+  filename: (req, file, callback) => {
+    callback(null, `${new Date().getTime()}-${file.originalname}`);
+  },
+});
+const upload = multer({ storage });
 
 initDB();
 
 const app = express();
 app.use(cors());
 app.use(logger('dev'));
+app.use(upload.single('file'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
