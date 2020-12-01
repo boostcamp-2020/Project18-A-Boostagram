91ba5dbf3a1fb425748e37eed24b5fbfdbdb9beb
chahtk <chahtk@gmail.com>
2020-12-01 17:30:07 +0900

    feat: 이미지 업로드 프로토타입
    - /test에 라우팅되어있음
    - 클라이언트로부터 file 배열을 받으면 저장한다
    - 그 후 object storage에 업로드하고 파일을 삭제

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import AWS from 'aws-sdk';
+import fs from 'fs';
+import path from 'path';
 import { indexController } from '../controllers';
 import feedRouter from './feed.router';
 
@@ -6,5 +9,38 @@ const router = express.Router();
 
 router.get('/', indexController);
 router.use('/feed', feedRouter);
+router.use('/test', (req, res) => {
+  const { accessKey, secretKey, ncloud, region } = process.env;
+  const bucketName = 'boostagram';
+  const endpoint = new AWS.Endpoint(ncloud || '');
+  const localFilePath = path.join(__dirname, '../../uploads/');
+
+  const S3 = new AWS.S3({
+    endpoint,
+    region,
+    credentials: {
+      accessKeyId: accessKey || '',
+      secretAccessKey: secretKey || '',
+    },
+  });
+  fs.readdir(localFilePath, (err, fileList) => {
+    const promiseArr = fileList.map(async (file) => {
+      return S3.putObject({
+        Bucket: bucketName,
+        Key: file,
+        ACL: 'public-read',
+        Body: fs.createReadStream(localFilePath + file),
+      })
+        .promise()
+        .then(() => fs.unlink(localFilePath + file, () => undefined));
+    });
+    Promise.all(promiseArr).then(async () => {
+      const resArr = fileList.map((file) => {
+        return `${process.env.ncloud}/${bucketName}/${file}`;
+      });
+      res.status(200).json(resArr);
+    });
+  });
+});
 
 export default router;
