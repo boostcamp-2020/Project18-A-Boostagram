3ec874871a62e96825ee2007708e36cc766c5430
chahtk <chahtk@gmail.com>
2020-12-03 04:59:34 +0900

    style: 함수 위치 변경

--- client/src/components/AouthTest.js
+++ client/src/components/AouthTest.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
+const handleButton = () => {
+  fetch('http://localhost:3000/login/github')
+    .then((res) => res.json())
+    .then((url) => {
+      document.location.href = url;
+    });
+};
+
 const AouthTest = () => {
-  const handleButton = () => {
-    fetch('http://localhost:3000/login/github')
-      .then((res) => res.json())
-      .then((url) => {
-        document.location.href = url;
-      });
-  };
   return (
     <div>
       <button type="button" onClick={handleButton}>
