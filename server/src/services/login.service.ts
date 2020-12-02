d40a6fc46d76d19e74f3f0acca8597abdf0f048d
chahtk <chahtk@gmail.com>
2020-12-02 23:08:39 +0900

    feat: login service
    - getToken: get access token
    - getUserData: get user data by token
    - createUser: create new user by github id
    - findUser: find user by userName in db

---
+++ server/src/services/login.service.ts
@@ -0,0 +1,52 @@
+import request from 'request';
+import UserModel from '../models/user.model';
+
+const { CLIENT_ID, CLIENT_SECRET } = process.env;
+const TOKEN_URL = 'https://github.com/login/oauth/access_token';
+const USER_URL = 'https://api.github.com/user';
+
+export const getToken = (code: any): any => {
+  const options = {
+    uri: TOKEN_URL,
+    method: 'POST',
+    body: {
+      code,
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+    },
+    json: true,
+  };
+  return new Promise((resolve, reject) =>
+    request.post(options, (err, res, body) => {
+      if (err) reject(err);
+      resolve(body.access_token);
+    }),
+  );
+};
+
+export const getUserData = (accessToken: string): Promise<string> => {
+  const options = {
+    headers: {
+      Authorization: `token ${accessToken}`,
+      'user-agent': 'node.js',
+    },
+  };
+  return new Promise((resolve, reject) => {
+    request.get(USER_URL, options, (err, res, body) => resolve(body));
+  });
+};
+
+export const createUser = async (
+  userName: string,
+  profileImg: string,
+): Promise<any> => {
+  const user = new UserModel({ userName, profileImg });
+  const result = await user.createUser();
+  return result;
+};
+
+export const findUser = async (userName: string): Promise<any> => {
+  const user = new UserModel({ userName });
+  const result = await user.findUserName();
+  return result;
+};
