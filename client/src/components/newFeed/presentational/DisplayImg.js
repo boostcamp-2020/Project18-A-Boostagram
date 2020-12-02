78de06b6b578b7616668558e80f0fee3980e962c
chahtk <chahtk@gmail.com>
2020-12-02 10:03:51 +0900

    style: 이미지 크기 맞추기
    - object-fit 사용
    - 애니메이션 작업을 위한 밑작업 진행중

--- client/src/components/newFeed/presentational/DisplayImg.js
+++ client/src/components/newFeed/presentational/DisplayImg.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 import PropTypes from 'prop-types';
+
+const imgMoveAnimation = keyframes`
+  from {
+    margin-left: 100%;
+  }
+  to {
+    margin-left: 0%;
+  }
+`;
 
 const style = {};
 style.DisplayImgContainer = styled.div`
-  position: absolute;
   width: 100%;
   height: 100%;
   ${(props) =>
@@ -18,6 +26,9 @@ style.DisplayImgContainer = styled.div`
 style.DisplayImg = styled.img`
   width: 100%;
   height: 100%;
+  object-fit: contain;
+  position: absolute;
+  margin-left: ${({ margin }) => margin};
 `;
 style.ArrowContainer = styled.div`
   width: 100%;
@@ -38,11 +49,25 @@ style.RightArrow = styled.button`
   position: absolute;
   right: 0;
 `;
+style.imgContainer = styled.div`
+  // todo: animation
+  /* animation: ${imgMoveAnimation} 0.5s; */
+`;
 const DisplayImg = (props) => {
   const { feedImgs, hover, handleImg, imgIndex = 0 } = props;
   return (
     <style.DisplayImgContainer hover={hover}>
-      {feedImgs.length !== 0 && <style.DisplayImg src={feedImgs[imgIndex]} />}
+      {feedImgs.length !== 0 && (
+        <style.imgContainer>
+          {imgIndex - 1 >= 0 && (
+            <style.DisplayImg src={feedImgs[imgIndex - 1]} margin="-100%" />
+          )}
+          <style.DisplayImg src={feedImgs[imgIndex]} />
+          {imgIndex + 1 < feedImgs.length && (
+            <style.DisplayImg src={feedImgs[imgIndex + 1]} margin="100%" />
+          )}
+        </style.imgContainer>
+      )}
       <style.ArrowContainer>
         {imgIndex !== 0 && (
           <button onClick={handleImg} type="button">
