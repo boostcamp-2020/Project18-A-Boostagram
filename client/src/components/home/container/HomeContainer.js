ef5aa9e9544a82ad66c6cbee03cd1f2f9ffe07de
BK <rlaqudrnr810@naver.com>
2020-12-02 22:47:48 +0900

    feat: client api 연동
    - /feed/following/ api 사용
    - dummy data 부분 삭제
    - container에서 fetch 후 data 전달

--- client/src/components/home/container/HomeContainer.js
+++ client/src/components/home/container/HomeContainer.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Side from '@home/presentational/Side';
 import Contents from '@home/presentational/Contents';
+import pathURL from '@constants/path';
 
 const style = {};
 
@@ -13,12 +14,45 @@ style.HomeContainer = styled.div`
   margin-top: 30px;
 `;
 const HomeContainer = () => {
-  return (
-    <style.HomeContainer>
-      <Contents />
-      <Side />
-    </style.HomeContainer>
-  );
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  // const user = useContext(userContext);
+  const userId = '5fc73c8d690808276cab1a88';
+  console.log('1');
+  const getData = () => {
+    const url = pathURL.IP + pathURL.API_HOME_FEED + userId;
+    console.log(url);
+    const option = {
+      mode: 'cors',
+      method: 'GET',
+    };
+    console.log('2');
+    async function fetchUrl() {
+      const response = await fetch(url, option);
+      const json = await response.json();
+      console.log('2.5');
+      setData(json);
+      setLoading(true);
+    }
+    useEffect(() => {
+      console.log('?');
+      fetchUrl();
+    }, []);
+  };
+  console.log('3');
+  getData();
+  console.log('4');
+  if (loading) {
+    console.log(loading);
+    return (
+      <style.HomeContainer>
+        <Contents data={data} />
+        <Side />
+      </style.HomeContainer>
+    );
+  }
+  console.log('!');
+  return <>loading...</>;
 };
 
 export default HomeContainer;
