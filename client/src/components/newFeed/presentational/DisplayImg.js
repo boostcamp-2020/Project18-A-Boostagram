eb279ed8c5b8b6a5f7b20c6195d368074b14ac5e
chahtk <chahtk@gmail.com>
2020-12-02 11:47:41 +0900

    style: 불필요한 객체 제거

--- client/src/components/newFeed/presentational/DisplayImg.js
+++ client/src/components/newFeed/presentational/DisplayImg.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { keyframes, css } from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import PropTypes from 'prop-types';
 
 const imgMoveAnimation = keyframes`
