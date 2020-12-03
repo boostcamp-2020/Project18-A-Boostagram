cce26064a2cc4056609d6c4512841201ab9a74b0
chahtk <chahtk@gmail.com>
2020-12-03 20:53:30 +0900

    feat: login UI

---
+++ client/src/components/login/presentational/LoginButton.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import icon from '@constants/icon';
+
+const style = {};
+style.LoginButton = styled.button`
+  outline: none;
+  border: none;
+  color: white;
+  background-color: ${(props) => props.theme.color.dark};
+  width: 160px;
+  height: 30px;
+  border-radius: 5px;
+  display: flex;
+  padding-top: 4px;
+  padding-left: 20px;
+  margin: 80px auto;
+`;
+style.Text = styled.div`
+  margin-top: 2px;
+  margin-left: 4px;
+  font-size: 15px;
+`;
+
+const LoginButton = ({ handleButton }) => {
+  return (
+    <style.LoginButton onClick={handleButton}>
+      <icon.GitLogo />
+      <style.Text>GitHub Login</style.Text>
+    </style.LoginButton>
+  );
+};
+
+LoginButton.propTypes = {
+  handleButton: PropTypes.func.isRequired,
+};
+
+export default LoginButton;
