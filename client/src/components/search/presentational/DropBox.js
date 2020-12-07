224aa921f0dac5e4d65f689fc64220918e3c0b1d
chahtk <chahtk@gmail.com>
2020-12-07 20:27:22 +0900

    feat: modal 활성화, 프로필 이미지, 네임 추가

--- client/src/components/search/presentational/DropBox.js
+++ client/src/components/search/presentational/DropBox.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 
 const style = {};
 style.DropBox = styled.div`
+  display: ${({ value }) => (value !== '' ? 'block' : 'none')};
   position: absolute;
   width: 241px;
   background-color: white;
@@ -18,21 +19,48 @@ style.UserList = styled.ul`
   position: relative;
 `;
 style.UserItem = styled.li`
-  display: block;
+  display: flex;
   padding: 15px;
   border-bottom: 1px solid ${({ theme }) => theme.color.border};
   &:last-child {
     border-bottom: none;
   }
 `;
-const DropBox = ({ suggestUsers }) => {
+style.ProfileImg = styled.img`
+  position: relative;
+  top: 2px;
+  width: 32px;
+  height: 32px;
+  border-radius: 32px;
+`;
+style.Texts = styled.span`
+  position: relative;
+  top: 0;
+  margin-left: 14px;
+`;
+style.UserName = styled.section`
+  font-weight: 650;
+`;
+style.Name = styled.section`
+  font-weight: 450;
+  opacity: 50%;
+`;
+const DropBox = ({ suggestUsers, value }) => {
   return (
-    <style.DropBox>
+    <style.DropBox value={value}>
       <style.UserList>
         {suggestUsers.map((user) => {
-          const { userName } = user;
+          const { profileImg, userName, name } = user;
           const key = userName;
-          return <style.UserItem key={key}>{userName}</style.UserItem>;
+          return (
+            <style.UserItem key={key}>
+              <style.ProfileImg src={profileImg} />
+              <style.Texts>
+                <style.UserName>{userName}</style.UserName>
+                <style.Name>{name}</style.Name>
+              </style.Texts>
+            </style.UserItem>
+          );
         })}
       </style.UserList>
     </style.DropBox>
@@ -42,6 +70,7 @@ const DropBox = ({ suggestUsers }) => {
 DropBox.propTypes = {
   suggestUsers: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string))
     .isRequired,
+  value: PropTypes.string.isRequired,
 };
 
 export default DropBox;
