90f8cdda117145067aecd66718bc8f86c27df6e2
chahtk <chahtk@gmail.com>
2020-12-07 18:16:07 +0900

    feat: 문자열 fetch
    - textarea에 문자열 입력시 fetch
    - 뷰 분리

--- client/src/components/search/container/SearchBar.js
+++ client/src/components/search/container/SearchBar.js
@@ -1,60 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import Search from '../../../constants/search.svg';
-import ClearIcon from '../../../public/clear.png';
+import PATH from '@constants/path';
+import SearchInput from '@search/presentational/SearchInput';
+import SearchIcon from '@search/presentational/SearchIcon';
+import ClearIcon from '@search/presentational/ClearIcon';
 
 const style = {};
 style.SearchContainer = styled.div`
   position: relative;
 `;
-style.SearchInput = styled.input`
-  height: 20px;
-  flex: 1;
-  background-color: ${(props) => props.theme.color.background};
-  border: 1px solid ${(props) => props.theme.color.border};
-  border-radius: 3px;
-  outline: none;
-  padding: 3px;
-  text-align: ${({ focus }) => (focus ? 'left' : 'center')};
-  padding-left: ${({ focus }) => (focus ? '20px' : '0px')};
-`;
-style.SearchIcon = styled.span`
-  position: absolute;
-  left: ${({ focus }) => (focus ? '4px' : '46px')};
-  top: 8px;
-  width: 15px;
-  height: 15px;
-  background-image: url(${Search});
-  background-repeat: no-repeat;
-  background-size: 13px;
-  opacity: 50%;
-`;
-style.ClearIcon = styled.img`
-  display: ${({ focus }) => (focus ? 'block' : 'none')};
-  position: absolute;
-  right: 3px;
-  top: 8px;
-  width: 12px;
-  height: 12px;
-  border-radius: 25px;
-  margin-right: 3px;
-  opacity: 50%;
-`;
 
 const SearchBar = () => {
   const [focus, setFocus] = useState(false);
+  const [value, setValue] = useState('');
+  const [suggestUsers, setSuggestUsers] = useState([]);
+
   const handleFocus = () => setFocus(!focus);
+  const handleBlur = () => {
+    setValue('');
+    setFocus(!focus);
+  };
+  const handleValue = ({ target }) => setValue(target.value);
+
+  useEffect(() => {
+    if (value !== '') {
+      fetch(`${PATH.IP + PATH.API_SEARCH_USERS}${value}`)
+        .then((res) => res.json())
+        .then((users) => {
+          const equalPrevious =
+            JSON.stringify(users) === JSON.stringify(suggestUsers);
+          if (!equalPrevious) setSuggestUsers(users);
+        });
+    }
+  }, [value]);
+
   return (
     <style.SearchContainer>
-      <style.SearchInput
+      <SearchIcon focus={focus} />
+      <SearchInput
         focus={focus}
-        onFocus={handleFocus}
-        onBlur={handleFocus}
-        type="text"
-        placeholder="검색"
+        value={value}
+        handleFocus={handleFocus}
+        handleBlur={handleBlur}
+        handleValue={handleValue}
       />
-      <style.SearchIcon focus={focus} />
-      <style.ClearIcon src={ClearIcon} focus={focus} />
+      <ClearIcon focus={focus} src={ClearIcon} />
     </style.SearchContainer>
   );
 };
