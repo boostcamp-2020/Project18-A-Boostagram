bf9cd8c6b9a0877501950e9746e1a63276a34e7f
chahtk <chahtk@gmail.com>
2020-12-07 16:48:36 +0900

    style: 검색창 UI 수정

--- client/src/components/search/container/SearchBar.js
+++ client/src/components/search/container/SearchBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Search from '../../../constants/search.svg';
 import ClearIcon from '../../../public/clear.png';
@@ -14,28 +14,47 @@ style.SearchInput = styled.input`
   border: 1px solid ${(props) => props.theme.color.border};
   border-radius: 3px;
   outline: none;
-  text-align: center;
   padding: 3px;
+  text-align: ${({ focus }) => (focus ? 'left' : 'center')};
+  padding-left: ${({ focus }) => (focus ? '20px' : '0px')};
 `;
 style.SearchIcon = styled.span`
   position: absolute;
-  left: 46px;
-  top: 6px;
+  left: ${({ focus }) => (focus ? '4px' : '46px')};
+  top: 8px;
   width: 15px;
   height: 15px;
   background-image: url(${Search});
   background-repeat: no-repeat;
-  background-size: 14px;
+  background-size: 13px;
   opacity: 50%;
 `;
-style.ClearIcon = styled.img``;
+style.ClearIcon = styled.img`
+  display: ${({ focus }) => (focus ? 'block' : 'none')};
+  position: absolute;
+  right: 3px;
+  top: 8px;
+  width: 12px;
+  height: 12px;
+  border-radius: 25px;
+  margin-right: 3px;
+  opacity: 50%;
+`;
 
 const SearchBar = () => {
+  const [focus, setFocus] = useState(false);
+  const handleFocus = () => setFocus(!focus);
   return (
     <style.SearchContainer>
-      <style.SearchInput type="text" placeholder="검색" />
-      <style.SearchIcon />
-      <style.ClearIcon src={ClearIcon} />
+      <style.SearchInput
+        focus={focus}
+        onFocus={handleFocus}
+        onBlur={handleFocus}
+        type="text"
+        placeholder="검색"
+      />
+      <style.SearchIcon focus={focus} />
+      <style.ClearIcon src={ClearIcon} focus={focus} />
     </style.SearchContainer>
   );
 };
