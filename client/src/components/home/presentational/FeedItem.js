b225e50d5e798f7f22c0dbe9f2136e8a5d68c68f
chahtk <chahtk@gmail.com>
2020-12-08 21:00:57 +0900

    refactor: excuteTime 분리

--- client/src/components/home/presentational/FeedItem.js
+++ client/src/components/home/presentational/FeedItem.js
@@ -6,6 +6,7 @@ import Comment from '@home/presentational/Comment';
 import UserContext from '@context/user';
 import pathURI from '@constants/path';
 import IntersectionHook from '@hooks/Intersection';
+import excuteTime from '@utils/excuteTIme';
 
 const style = {};
 
@@ -178,32 +179,6 @@ const getCommentLength = (comments) => {
   return comments.length;
 };
 
-const excuteTime = (now) => {
-  const sec = 1000;
-  const min = sec * 60;
-  const hour = min * 60;
-  const day = hour * 24;
-  const week = day * 7;
-
-  const time = new Date() - new Date(now);
-
-  if (time < min) {
-    return `${Math.round(time / sec)}초 전`;
-  }
-
-  if (time < hour) {
-    return `${Math.round(time / min)}분 전`;
-  }
-
-  if (time < day) {
-    return `${Math.round(time / hour)}시간 전`;
-  }
-
-  if (time < week) {
-    return `${Math.round(time / day)}일 전`;
-  }
-  return `${Math.round(time / week)}주 전`;
-};
 const getLikeStatus = (like, userName) => {
   const result = like.find((element) => element.userName === userName);
   return result !== undefined;
