ed90c455a6004ff6d9adc5b253ded24ace6c1c4c
JuHyeon Lee <leon6095@gmail.com>
2020-12-08 14:47:16 +0900

    feat: 게시글 이미지 넘겨보기 기능 추가

--- client/src/components/feedDetail/container/FeedDetailContainer.js
+++ client/src/components/feedDetail/container/FeedDetailContainer.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext, useEffect } from 'react';
 import styled from 'styled-components';
 import ModalContext from '@context/modal';
+import ImgNav from '@newFeed/presentational/ImgNav';
 
 const style = {};
 
@@ -17,9 +18,35 @@ style.FeedDetailContainer = styled.div`
   margin-top: 35px;
 `;
 
+style.FeedImageContainer = styled.div`
+  width: 600px;
+  height: 600px;
+`;
+
 style.FeedImage = styled.img`
   width: 600px;
   height: 600px;
+`;
+
+style.ArrowContainer = styled.div`
+  width: 600px;
+  position: absolute;
+  top: 42%;
+  & > button {
+    outline: none;
+    border: none;
+    width: 20px;
+    height: 20px;
+    background-color: ${(props) => props.theme.color.background};
+    margin: 20px;
+    border-radius: 20px;
+    opacity: 70%;
+  }
+`;
+
+style.RightArrow = styled.button`
+  position: absolute;
+  right: 0;
 `;
 
 const FeedDetailContainer = ({ modalActive }) => {
@@ -27,14 +54,40 @@ const FeedDetailContainer = ({ modalActive }) => {
   const { _id, author, feedImg, content, like, comments } = selectedFeed;
 
   const [loading, setLoading] = useState(false);
+  const [selectedIndex, setImageIndex] = useState(0);
+
+  const nextImage = () => setImageIndex(selectedIndex + 1);
+  const beforeImage = () => setImageIndex(selectedIndex - 1);
+
+  // if (loading) console.log(selectedIndex, feedImg[selectedIndex]);
 
   useEffect(() => {
+    setImageIndex(0);
     if (Object.keys(selectedFeed).length > 0) setLoading(true);
   }, [selectedFeed]);
 
   return (
     <style.FeedDetailContainer active={modalActive}>
-      {loading ? <style.FeedImage src={feedImg[0]} /> : <>loading...</>}
+      {loading ? (
+        <style.FeedImageContainer>
+          <style.FeedImage src={feedImg[selectedIndex]} />
+          <ImgNav imgIndex={selectedIndex} imgs={feedImg} />
+          <style.ArrowContainer>
+            {selectedIndex !== 0 && (
+              <button onClick={beforeImage} type="button">
+                &lt;
+              </button>
+            )}
+            {selectedIndex < feedImg.length - 1 && (
+              <style.RightArrow onClick={nextImage} type="button">
+                &gt;
+              </style.RightArrow>
+            )}
+          </style.ArrowContainer>
+        </style.FeedImageContainer>
+      ) : (
+        <>loading...</>
+      )}
     </style.FeedDetailContainer>
   );
 };
