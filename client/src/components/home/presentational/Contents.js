730b0b0d7596fce3f5ce3adf9fedeb778e210032
JuHyeon Lee <leon6095@gmail.com>
2020-12-10 19:25:12 +0900

    feat: 안내 메세지 추가

--- client/src/components/home/presentational/Contents.js
+++ client/src/components/home/presentational/Contents.js
@@ -20,6 +20,7 @@ style.GuideMessage = styled.div`
   padding: 10px;
   align-content: center;
   justify-content: center;
+  margin-bottom: 10px;
 `;
 
 const Contents = (input) => {
@@ -29,10 +30,16 @@ const Contents = (input) => {
       <Story datas={data} />
       <Feed data={data} setGetMore={setGetMore} />
       {data.length === 0 ? (
-        <style.GuideMessage>
-          <icon.Explore />
-          &nbsp; 오른쪽 위 나침반 모양을 눌러 다른 사람들의 글을 읽어보세요!
-        </style.GuideMessage>
+        <>
+          <style.GuideMessage>
+            <icon.Home />
+            &nbsp; 홈 화면에서는 팔로우한 사용자들의 글을 볼 수 있어요!
+          </style.GuideMessage>
+          <style.GuideMessage>
+            <icon.Explore />
+            &nbsp; 오른쪽 위 나침반 모양을 눌러 다른 사람들의 글을 읽어보세요!
+          </style.GuideMessage>
+        </>
       ) : (
         <></>
       )}
