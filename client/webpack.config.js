b49e16215fbceefa0d94afdd5c5fd38f4f26f0b9
JuHyeon Lee <leon6095@gmail.com>
2020-12-11 14:14:45 +0900

    fix: favicon 배경 제거

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      favicon: './src/public/favicon_B.ico',
+      favicon: './src/public/favicon_B_nobg.ico',
     }),
     new CleanWebpackPlugin(),
   ],
