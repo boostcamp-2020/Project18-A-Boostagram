61bbdbdd60ee510877fab9d5e67d3817324871ad
BK <rlaqudrnr810@naver.com>
2020-12-14 21:03:00 +0900

    fix: 변수명 등 변경
    - 변수명 변경
    - 경로 수정

--- client/src/api/GetOneFeedAPI.js
+++ client/src/api/GetOneFeedAPI.js
@@ -1,9 +1,9 @@
 import pathURL from '@constants/path';
 
 const GetOneFeedAPI = (feedId) => {
-  return fetch(`${pathURL.IP + pathURL.API_NEWFEED}/${feedId}`).then((res) =>
-    res.json(),
-  );
+  return fetch(
+    `${pathURL.IP + pathURL.API_DETAIL_FEED}/${feedId}`,
+  ).then((res) => res.json());
 };
 
 export default GetOneFeedAPI;
